f017c8727a5c8964e097bfd118fc20b04cad193b
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-07-31 02:26:39 -0300

    classe de teste celsius em fahrenheit

---
+++ CelsiusToFahrenheit/src/testes/TempTest.java
@@ -0,0 +1,81 @@
+package testes;
+
+import static org.junit.Assert.*;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import org.junit.Test;
+
+public class TempTest {
+
+	private WebDriver driver;
+	  private String baseUrl;
+	  private boolean acceptNextAlert = true;
+	  private StringBuffer verificationErrors = new StringBuffer();
+
+	  @Before
+	  public void setUp() throws Exception {
+	    driver = new FirefoxDriver();
+	    baseUrl = "http://localhost:8080/";
+	    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	  }
+
+	  @Test
+	  public void testTemp() throws Exception {
+	    driver.get(baseUrl + "/CelsiusToFahrenheit/");
+	    new Select(driver.findElement(By.name("tempOpt"))).selectByVisibleText("Fahrenheit");
+	    driver.findElement(By.name("temperatura")).clear();
+	    driver.findElement(By.name("temperatura")).sendKeys("100");
+	    driver.findElement(By.cssSelector("input[type=\"submit\"]")).click();
+	    assertEquals("Valor convertido: 212.0", driver.findElement(By.cssSelector("h1")).getText());
+	  }
+
+	  @After
+	  public void tearDown() throws Exception {
+	    driver.quit();
+	    String verificationErrorString = verificationErrors.toString();
+	    if (!"".equals(verificationErrorString)) {
+	      fail(verificationErrorString);
+	    }
+	  }
+
+	  private boolean isElementPresent(By by) {
+	    try {
+	      driver.findElement(by);
+	      return true;
+	    } catch (NoSuchElementException e) {
+	      return false;
+	    }
+	  }
+
+	  private boolean isAlertPresent() {
+	    try {
+	      driver.switchTo().alert();
+	      return true;
+	    } catch (NoAlertPresentException e) {
+	      return false;
+	    }
+	  }
+
+	  private String closeAlertAndGetItsText() {
+	    try {
+	      Alert alert = driver.switchTo().alert();
+	      String alertText = alert.getText();
+	      if (acceptNextAlert) {
+	        alert.accept();
+	      } else {
+	        alert.dismiss();
+	      }
+	      return alertText;
+	    } finally {
+	      acceptNextAlert = true;
+	    }
+	  }
+
+}
