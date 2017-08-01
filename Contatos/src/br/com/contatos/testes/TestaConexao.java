15aad94108aaa634d9216f612130dee36d979434
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-01 01:53:59 -0300

    classe para testar a conexão

---
+++ Contatos/src/br/com/contatos/testes/TestaConexao.java
@@ -0,0 +1,23 @@
+package br.com.contatos.testes;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import br.com.contatos.jdbc.ConnectionFactory;
+
+public class TestaConexao {
+
+	public static void main(String[] args) {
+		
+		Connection connection = new ConnectionFactory().getConnection();
+		System.out.println("Conexão aberta!");
+		
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+}
