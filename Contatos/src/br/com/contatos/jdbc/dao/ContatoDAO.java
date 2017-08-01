a57fc73462e2e1e340f27b2f70f9f3df063e7b89
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-01 02:46:30 -0300

    adicionado método para listar os contatos

--- Contatos/src/br/com/contatos/jdbc/dao/ContatoDAO.java
+++ Contatos/src/br/com/contatos/jdbc/dao/ContatoDAO.java
@@ -3,7 +3,11 @@ package br.com.contatos.jdbc.dao;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import br.com.contatos.jdbc.ConnectionFactory;
 import br.com.contatos.jdbc.modelo.Contato;
@@ -36,5 +40,38 @@ public class ContatoDAO {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public List<Contato> getListaContatos() {
+	     try {
+	         List<Contato> contatos = new ArrayList<Contato>();
+	         
+	         PreparedStatement stmt = this.connection.prepareStatement
+	         ("select * from contatos");
+	         
+	         ResultSet rs = stmt.executeQuery();
+	 
+	         while (rs.next()) {
+	             Contato contato = new Contato();
+	             contato.setId(rs.getLong("id"));
+	             contato.setNome(rs.getString("nome"));
+	             contato.setEmail(rs.getString("email"));
+	             contato.setEndereco(rs.getString("endereco"));
+	 
+	             Calendar data = Calendar.getInstance();
+	             data.setTime(rs.getDate("dataNascimento"));
+	             contato.setDataNascimento(data);
+	 
+	             contatos.add(contato);
+	         }
+	         
+	         rs.close();
+	         stmt.close();
+	         
+	         return contatos;
+	         
+	     } catch (SQLException e) {
+	         throw new RuntimeException(e);
+	     }
+	 }
 
 }
