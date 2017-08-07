ad0961b90b6a27c4b563e866df081e56c080f8e6
Felipe Sobreira <felipesobreira.c@hotmail.com>
2017-08-07 02:32:14 -0300

    interface para Palavra

---
+++ TradutorSimples/src/model/Palavra.java
@@ -0,0 +1,10 @@
+package model;
+
+public interface Palavra {
+	
+	public String getNome();
+	public String getTraducao();
+	public void setNome( String nome);
+	public void setTraducao(String traducao);
+
+}
